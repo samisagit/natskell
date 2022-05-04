68e513530d38bd65f69a41adf57c90f66cd50d8e
samisagit <sam@whiteteam.co.uk>
2022-05-04 21:43:40 +0100

    Refactor tests with many cases
    Use tabular tests to increase readability

--- test/ErrSpec.hs
+++ test/ErrSpec.hs
@@ -2,121 +2,43 @@
 
 module ErrSpec (spec) where
 
+import           Control.Monad
+import           Data.ByteString
 import           Err
 import           Parser
 import           Test.Hspec
+import           Text.Printf
+
+cases :: [(ByteString, Err)]
+cases = [
+  ("-ERR 'Unknown Protocol Operation'\r\n", Err "Unknown Protocol Operation" True),
+  ("-ERR 'Attempted To Connect To Route Port'\r\n", Err "Attempted To Connect To Route Port" True),
+  ("-ERR 'Authorization Violation'\r\n", Err "Authorization Violation" True),
+  ("-ERR 'Authorization Timeout'\r\n", Err "Authorization Timeout" True),
+  ("-ERR 'Invalid Client Protocol'\r\n", Err "Invalid Client Protocol" True),
+  ("-ERR 'Maximum Control Line Exceeded'\r\n", Err "Maximum Control Line Exceeded" True),
+  ("-ERR 'Parser Error'\r\n", Err "Parser Error" True),
+  ("-ERR 'Secure Connection - TLS Required'\r\n", Err "Secure Connection - TLS Required" True),
+  ("-ERR 'Stale Connection'\r\n", Err "Stale Connection" True),
+  ("-ERR 'Maximum Connections Exceeded'\r\n", Err "Maximum Connections Exceeded" True),
+  ("-ERR 'Slow Consumer'\r\n", Err "Slow Consumer" True),
+  ("-ERR 'Maximum Payload Violation'\r\n", Err "Maximum Payload Violation" True),
+  ("-ERR 'Invalid Subject'\r\n", Err "Invalid Subject" False),
+  ("-ERR 'Permissions Violation For Subscription To FOO.'\r\n", Err "Permissions Violation For Subscription To FOO." False),
+  ("-ERR 'Permissions Violation For Publish To FOO.'\r\n", Err "Permissions Violation For Publish To FOO." False)
+  ]
 
 spec :: Spec
 spec = do
-  manual
+  form
 
-manual :: Spec
-manual = do
+form = do
   describe "parser" $ do
-    it "correctly parses -ERR 'Unknown Protocol Operation'" $ do
-      do
-        let result = fmap fst unknownOp
-        result `shouldBe` Just (Err "Unknown Protocol Operation" True)
-        let left = fmap snd unknownOp
+    forM_ cases $ \(input, expected) ->
+      it (printf "correctly parses %s" (show input)) $ do
+        let output = runParser parser input
+        let result = fmap fst output
+        result `shouldBe` Just expected
+        let left = fmap snd output
         left `shouldBe` Just ""
-    it "correctly parses -ERR 'Attempted To Connect To Route Port'" $ do
-      do
-        let result = fmap fst routePortConn
-        result `shouldBe` Just (Err "Attempted To Connect To Route Port" True)
-        let left = fmap snd routePortConn
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Authorization Violation'" $ do
-      do
-        let result = fmap fst authViolation
-        result `shouldBe` Just (Err "Authorization Violation" True)
-        let left = fmap snd authViolation
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Authorization Timeout'" $ do
-      do
-        let result = fmap fst authTimeout
-        result `shouldBe` Just (Err "Authorization Timeout" True)
-        let left = fmap snd authTimeout
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Invalid Client Protocol'" $ do
-      do
-        let result = fmap fst invalidClientProtocol
-        result `shouldBe` Just (Err "Invalid Client Protocol" True)
-        let left = fmap snd invalidClientProtocol
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Maximum Control Line Exceeded'" $ do
-      do
-        let result = fmap fst maxControlLineExceeded
-        result `shouldBe` Just (Err "Maximum Control Line Exceeded" True)
-        let left = fmap snd maxControlLineExceeded
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Parser Error'" $ do
-      do
-        let result = fmap fst parseErr
-        result `shouldBe` Just (Err "Parser Error" True)
-        let left = fmap snd parseErr
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Secure Connection - TLS Required'" $ do
-      do
-        let result = fmap fst _TLSRequired
-        result `shouldBe` Just (Err "Secure Connection - TLS Required" True)
-        let left = fmap snd _TLSRequired
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Stale Connection'" $ do
-      do
-        let result = fmap fst staleConnection
-        result `shouldBe` Just (Err "Stale Connection" True)
-        let left = fmap snd staleConnection
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Maximum Connections Exceeded'" $ do
-      do
-        let result = fmap fst maxConnExceeded
-        result `shouldBe` Just (Err "Maximum Connections Exceeded" True)
-        let left = fmap snd maxConnExceeded
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Slow Consumer'" $ do
-      do
-        let result = fmap fst slowConsumer
-        result `shouldBe` Just (Err "Slow Consumer" True)
-        let left = fmap snd slowConsumer
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Maximum Payload Violation'" $ do
-      do
-        let result = fmap fst maxPayloadViolation
-        result `shouldBe` Just (Err "Maximum Payload Violation" True)
-        let left = fmap snd maxPayloadViolation
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Invalid Subject'" $ do
-      do
-        let result = fmap fst invalidSubj
-        result `shouldBe` Just (Err "Invalid Subject" False)
-        let left = fmap snd invalidSubj
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Permissions Violation for Subscription to FOO.'" $ do
-      do
-        let result = fmap fst subPermViolation
-        result `shouldBe` Just (Err "Permissions Violation for Subscription to FOO." False)
-        let left = fmap snd subPermViolation
-        left `shouldBe` Just ""
-    it "correctly parses -ERR 'Permissions Violation for Publish to FOO.'" $ do
-      do
-        let result = fmap fst pubPermViolation
-        result `shouldBe` Just (Err "Permissions Violation for Publish to FOO." False)
-        let left = fmap snd pubPermViolation
-        left `shouldBe` Just ""
-  where
-    unknownOp = runParser parser "-ERR 'Unknown Protocol Operation'\r\n"
-    routePortConn = runParser parser "-ERR 'Attempted To Connect To Route Port'\r\n"
-    authViolation = runParser parser "-ERR 'Authorization Violation'\r\n"
-    authTimeout = runParser parser "-ERR 'Authorization Timeout'\r\n"
-    invalidClientProtocol = runParser parser "-ERR 'Invalid Client Protocol'\r\n"
-    maxControlLineExceeded = runParser parser "-ERR 'Maximum Control Line Exceeded'\r\n"
-    parseErr = runParser parser "-ERR 'Parser Error'\r\n"
-    _TLSRequired = runParser parser "-ERR 'Secure Connection - TLS Required'\r\n"
-    staleConnection = runParser parser "-ERR 'Stale Connection'\r\n"
-    maxConnExceeded = runParser parser "-ERR 'Maximum Connections Exceeded'\r\n"
-    slowConsumer = runParser parser "-ERR 'Slow Consumer'\r\n"
-    maxPayloadViolation = runParser parser "-ERR 'Maximum Payload Violation'\r\n"
-    invalidSubj = runParser parser "-ERR 'Invalid Subject'\r\n"
-    subPermViolation = runParser parser "-ERR 'Permissions Violation for Subscription to FOO.'\r\n"
-    pubPermViolation = runParser parser "-ERR 'Permissions Violation for Publish to FOO.'\r\n"
 
--- test/MsgSpec.hs
+++ test/MsgSpec.hs
@@ -2,81 +2,34 @@
 
 module MsgSpec (spec) where
 
+import           Control.Monad
+import           Data.ByteString
 import           Msg
 import           Parser
 import           Test.Hspec
+import           Text.Printf
+
+cases :: [(ByteString, Data)]
+cases = [
+  ("MSG FOO 13 BAR 17\r\nsome payload bits\r\n", Data "FOO" 13 (Just "BAR") 17 (Just "some payload bits")),
+  ("MSG FOO 13 BAR 0\r\n", Data "FOO" 13 (Just "BAR") 0 Nothing),
+  ("MSG FOO 13 17\r\nsome payload bits\r\n", Data "FOO" 13 Nothing 17 (Just"some payload bits")),
+  ("MSG FOO 13 0\r\n", Data "FOO" 13 Nothing 0 Nothing),
+  ("MSG FOO 13 20\r\nmulti\r\nline\r\npayload\r\n", Data "FOO" 13 Nothing 20 (Just "multi\r\nline\r\npayload")),
+  ("MSG FOO.BAR.BAZ 13 IN.*.BOX.> 0\r\n", Data "FOO.BAR.BAZ" 13 (Just "IN.*.BOX.>") 0 Nothing)
+  ]
+
+form = do
+  describe "parser" $ do
+    forM_ cases $ \(input, expected) ->
+      it (printf "correctly parses %s" (show input)) $ do
+        let output = runParser parser input
+        let result = fmap fst output
+        result `shouldBe` Just expected
+        let left = fmap snd output
+        left `shouldBe` Just ""
 
 spec :: Spec
 spec = do
-  manual
+  form
 
-manual :: Spec
-manual = do
-  describe "parser" $ do
-    it "correctly parses MSG with reply-to and payload" $ do
-      do
-        let result = fmap fst withReplyAndPayload
-        fmap subject result `shouldBe` Just "FOO"
-        fmap sid result `shouldBe` Just 13
-        fmap replyTo result `shouldBe` Just (Just "BAR")
-        fmap byteCount result `shouldBe` Just 17
-        fmap payload result `shouldBe` Just (Just "some payload bits")
-        let left = fmap snd withReplyAndPayload
-        left `shouldBe` Just ""
-    it "correctly parses MSG with reply-to " $ do
-      do
-        let result = fmap fst withReply
-        fmap subject result `shouldBe` Just "FOO"
-        fmap sid result `shouldBe` Just 13
-        fmap replyTo result `shouldBe` Just (Just "BAR")
-        fmap byteCount result `shouldBe` Just 0
-        fmap payload result `shouldBe` Just Nothing
-        let left = fmap snd withReply
-        left `shouldBe` Just ""
-    it "correctly parses MSG with payload" $ do
-      do
-        let result = fmap fst withPayload
-        fmap subject result `shouldBe` Just "FOO"
-        fmap sid result `shouldBe` Just 13
-        fmap replyTo result `shouldBe` Just Nothing
-        fmap byteCount result `shouldBe` Just 17
-        fmap payload result `shouldBe` Just (Just "some payload bits")
-        let left = fmap snd withPayload
-        left `shouldBe` Just ""
-    it "correctly parses minimal MSG" $ do
-      do
-        let result = fmap fst min
-        fmap subject result `shouldBe` Just "FOO"
-        fmap sid result `shouldBe` Just 13
-        fmap replyTo result `shouldBe` Just Nothing
-        fmap byteCount result `shouldBe` Just 0
-        fmap payload result `shouldBe` Just Nothing
-        let left = fmap snd min
-        left `shouldBe` Just ""
-    it "correctly parses MSG with multi line payload" $ do
-      do
-        let result = fmap fst withMultiLinePayload
-        fmap subject result `shouldBe` Just "FOO"
-        fmap sid result `shouldBe` Just 13
-        fmap replyTo result `shouldBe` Just Nothing
-        fmap byteCount result `shouldBe` Just 20
-        fmap payload result `shouldBe` Just (Just "multi\r\nline\r\npayload")
-        let left = fmap snd withMultiLinePayload
-        left `shouldBe` Just ""
-    it "correctly parses MSG with nested subjects" $ do
-      do
-        let result = fmap fst withNestedSubjects
-        fmap subject result `shouldBe` Just "FOO.BAR.BAZ"
-        fmap sid result `shouldBe` Just 13
-        fmap replyTo result `shouldBe` Just (Just "IN.*.BOX.>")
-        fmap byteCount result `shouldBe` Just 0
-        fmap payload result `shouldBe` Just Nothing
-        let left = fmap snd withNestedSubjects
-        left `shouldBe` Just ""
-  where
-    withReplyAndPayload = runParser parser "MSG FOO 13 BAR 17\r\nsome payload bits\r\n"
-    withReply = runParser parser "MSG FOO 13 BAR 0\r\n"
-    withPayload = runParser parser "MSG FOO 13 17\r\nsome payload bits\r\n"
-    min = runParser parser "MSG FOO 13 0\r\n"
-    withMultiLinePayload = runParser parser "MSG FOO 13 20\r\nmulti\r\nline\r\npayload\r\n"
-    withNestedSubjects = runParser parser "MSG FOO.BAR.BAZ 13 IN.*.BOX.> 0\r\n"
