72ad53f284c50b2dcd532826eaea002194ecb73b
samisagit <sam@whiteteam.co.uk>
2022-05-09 21:08:10 +0100

    Remove redundant do scope

--- test/OkSpec.hs
+++ test/OkSpec.hs
@@ -14,10 +14,9 @@ manual :: Spec
 manual = do
   describe "parser" $ do
     it "correctly parses +OK" $ do
-      do
-        let result = fmap fst ok
-        result `shouldBe` Just Ok
-        let left = fmap snd ok
-        left `shouldBe` Just ""
+      let result = fmap fst ok
+      result `shouldBe` Just Ok
+      let left = fmap snd ok
+      left `shouldBe` Just ""
   where
     ok = runParser parser "+OK\r\n"
