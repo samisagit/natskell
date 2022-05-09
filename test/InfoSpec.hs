3fbdc6fcdae63a4773884aff76b5fe7dcad8609d
samisagit <sam@whiteteam.co.uk>
2022-05-09 21:07:08 +0100

    Add new lines to relevant test cases

--- test/InfoSpec.hs
+++ test/InfoSpec.hs
@@ -17,17 +17,17 @@ spec = do
 cases :: [(BS.ByteString, Data, String)]
 cases = [
   (
-    "INFO {\"server_id\": \"some-server\", \"version\": \"semver\", \"go\": \"1.13\", \"host\": \"127.0.0.1\", \"port\": 4222, \"max_payload\": 1024, \"proto\": 3, \"client_id\": 1, \"auth_required\": true, \"tls_required\": true, \"connect_urls\": [\"https://127.0.0.1:4222\"], \"ldm\": true}",
+    "INFO {\"server_id\": \"some-server\", \"version\": \"semver\", \"go\": \"1.13\", \"host\": \"127.0.0.1\", \"port\": 4222, \"max_payload\": 1024, \"proto\": 3, \"client_id\": 1, \"auth_required\": true, \"tls_required\": true, \"connect_urls\": [\"https://127.0.0.1:4222\"], \"ldm\": true}\r\n",
     Data "some-server" "semver" "1.13" "127.0.0.1" 4222 1024 3 (Just 1) True True (Just ["https://127.0.0.1:4222"]) True,
     "fully populated"
   ),
   (
-    "INFO {\"server_id\": \"some-server\", \"version\": \"semver\", \"go\": \"1.13\", \"host\": \"127.0.0.1\", \"port\": 4222, \"max_payload\": 1024, \"proto\": 3, \"auth_required\": true, \"tls_required\": true, \"ldm\": true}",
+    "INFO {\"server_id\": \"some-server\", \"version\": \"semver\", \"go\": \"1.13\", \"host\": \"127.0.0.1\", \"port\": 4222, \"max_payload\": 1024, \"proto\": 3, \"auth_required\": true, \"tls_required\": true, \"ldm\": true}\r\n",
     Data "some-server" "semver" "1.13" "127.0.0.1" 4222 1024 3 Nothing True True Nothing True,
     "minimal"
   ),
   (
-    "INFO {\"server_id\": \"some-server\", \"version\": \"semver\", \"go\": \"1.13\", \"host\": \"127.0.0.1\", \"port\": 4222, \"max_payload\": 1024, \"proto\": 3, \"client_id\": 1, \"auth_required\": true, \"tls_required\": true, \"connect_urls\": [\"https://127.0.0.1:4222\", \"https://192.168.9.7:4222\"], \"ldm\": true}",
+    "INFO {\"server_id\": \"some-server\", \"version\": \"semver\", \"go\": \"1.13\", \"host\": \"127.0.0.1\", \"port\": 4222, \"max_payload\": 1024, \"proto\": 3, \"client_id\": 1, \"auth_required\": true, \"tls_required\": true, \"connect_urls\": [\"https://127.0.0.1:4222\", \"https://192.168.9.7:4222\"], \"ldm\": true}\r\n",
     Data "some-server" "semver" "1.13" "127.0.0.1" 4222 1024 3 (Just 1) True True (Just ["https://127.0.0.1:4222", "https://192.168.9.7:4222"]) True,
     "multi connect urls"
   )
