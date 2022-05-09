92f14ceb51001863b5eda5bfeefa97b889d2bb5e
samisagit <sam@whiteteam.co.uk>
2022-05-09 22:45:08 +0100

    Add SUB transformer

---
+++ test/SubSpec.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module SubSpec (spec) where
+
+import           Control.Monad
+import           Data.ByteString
+import           Sub
+import           Test.Hspec
+import           Text.Printf
+
+spec :: Spec
+spec = do
+  manual
+
+cases :: [(Data, ByteString, String)]
+cases = [
+  (Data "SOME.SUBJ" Nothing "uuid", "SUB SOME.SUBJ uuid", "without queue group"),
+  (Data "SOME.SUBJ" (Just "QUEUE.GROUP.A") "uuid", "SUB SOME.SUBJ QUEUE.GROUP.A uuid", "with queue group")
+  ]
+
+manual = do
+  forM_ cases $ \(input, expected, caseName) ->
+    it (printf "correctly transforms %s" caseName) $ do
+      transform input `shouldBe` expected
