b32ce1c7e94efaa77b2cd90217b804f25462698a
samisagit <sam@whiteteam.co.uk>
2022-05-09 21:08:27 +0100

    Add PING and PONG transformers

--- test/PingSpec.hs
+++ test/PingSpec.hs
@@ -14,10 +14,12 @@ manual :: Spec
 manual = do
   describe "parser" $ do
     it "correctly parses PING" $ do
-      do
-        let result = fmap fst ping
-        result `shouldBe` Just Ping
-        let left = fmap snd ping
-        left `shouldBe` Just ""
+      let result = fmap fst ping
+      result `shouldBe` Just Ping
+      let left = fmap snd ping
+      left `shouldBe` Just ""
+  describe "transformer" $ do
+    it "correctly transforms to 'PING'" $ do
+      transform Ping `shouldBe` "PING"
   where
     ping = runParser parser "PING\r\n"
