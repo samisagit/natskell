7560bb53872042097da8f6a0a76b5ed7249ee889
samisagit <sam@whiteteam.co.uk>
2022-06-19 10:38:47 +0000

    Use health check to make sure NATS is running
    System tests need to have a running NATS server up and running
    before the test case starts. Using the HTTP health check we can
    be fairly sure it's there.

--- test/ClientSpec.hs
+++ test/ClientSpec.hs
@@ -2,20 +2,21 @@ module ClientSpec (spec) where
 
 import           Client
 import           Control.Exception
-import qualified Docker.Client     as DC
+import qualified Docker.Client      as DC
 import           NatsWrappers
+import qualified Network.Simple.TCP as TCP
 import           Test.Hspec
 
 spec :: Spec
 spec = do
   sys
 
-withNATSConnection :: (DC.ContainerID -> IO ()) -> IO ()
+withNATSConnection :: ((DC.ContainerID, String, Int) -> IO ()) -> IO ()
 withNATSConnection = bracket startNATS stopNATS
 
 sys = do
   around withNATSConnection $ do
     describe "Client" $ do
       describe "systest" $ do
-        it "connects successfully" $ \_ -> do
-          connect $ Api "0.0.0.0" "4222"
+        it "connects successfully" $ \(_, host, port) -> do
+          connect host port 10
--- test/NatsWrappers.hs
+++ test/NatsWrappers.hs
@@ -4,77 +4,88 @@ module NatsWrappers where
 
 import           Control.Concurrent
 import           Control.Exception
-import qualified Docker.Client      as DC
-import qualified Network.Simple.TCP as TCP
+import qualified Data.Text
+import qualified Data.Text           as Text
+import qualified Docker.Client       as DC
+import qualified Docker.Client.Types as DCT
+import qualified Network.HTTP        as HTTP
+import qualified Network.Simple.TCP  as TCP
 
 second = 1000000
 
-runNATSContainer :: IO DC.ContainerID
+runNATSContainer :: IO (DC.ContainerID, DCT.ContainerDetails)
 runNATSContainer = do
   h <- DC.unixHttpHandler "/var/run/docker.sock"
   DC.runDockerT (DC.defaultClientOpts, h) $
-    do let pb = DC.PortBinding 4222 DC.TCP [DC.HostPort "0.0.0.0" 4222]
-       let createOpts = DC.addPortBinding pb $ DC.defaultCreateOpts "nats:latest"
-       cid <- DC.createContainer createOpts Nothing
-       case cid of
-         Left err -> error $ show err
-         Right i -> do
-           e <- DC.startContainer DC.defaultStartOpts i
-           case e of
-             Left err -> error $ show err
-             Right _  -> do
-               return i
+    do
+      let hpa = DC.HostPort "0.0.0.0" 0
+      let pba = DC.PortBinding 4222 DC.TCP [hpa]
 
-ensureNATS :: DC.ContainerID -> IO DC.ContainerID
-ensureNATS id = do
-  _ <- ensureNATSIsListening 10
-  return id
+      let hpb = DC.HostPort "0.0.0.0" 0
+      let pbb = DC.PortBinding 8222 DC.TCP [hpb]
 
-ensureNATSIsListening :: Int -> IO ()
-ensureNATSIsListening retryCount = do
-  result <- try(
-    TCP.connect "0.0.0.0" "4222" $ \(sock, _) -> do
-      return sock
-    ) :: IO (Either SomeException TCP.Socket)
+      let createOpts = DC.addPortBinding pba $ DC.addPortBinding pbb $ DC.defaultCreateOpts "nats:latest"
+      cid <- DC.createContainer createOpts Nothing
+      case cid of
+        Left err -> error $ show err
+        Right i -> do
+          e <- DC.startContainer DC.defaultStartOpts i
+          case e of
+            Left err -> error $ show err
+            Right _  -> do
+              details <- DC.inspectContainer i
+              case details of
+                Left err -> error $ show err
+                Right d  -> return (i, d)
+
+ensureNATS :: (DC.ContainerID, DCT.ContainerDetails) -> IO (DC.ContainerID, String, Int)
+ensureNATS (id, d) = do
+  ensureNATSIsListening hostPortB 10
+  return (id, (Text.unpack $ DCT.hostIp hostPortA), (fromIntegral $ DCT.hostPost hostPortA))
+  where
+    networkPortA = (DCT.networkSettingsPorts $ DCT.networkSettings d) !! 2
+    networkPortB = (DCT.networkSettingsPorts $ DCT.networkSettings d) !! 0
+    hostPortA = head $ DCT.hostPorts networkPortA
+    hostPortB = head $ DCT.hostPorts networkPortB
+
+callNATSHealth hp retryCount = do
+  res <- HTTP.simpleHTTP (HTTP.getRequest $ "http://" ++ (Text.unpack $ DC.hostIp hp) ++ ":" ++  (show $ DC.hostPost hp))
+  case res of
+    Left e -> do
+      if retryCount == 0
+        then error $ "retried too many times " ++ show e
+      else do
+        threadDelay $ second `div` 10
+        callNATSHealth hp $ retryCount - 1
+    Right res -> do
+      return $ HTTP.rspCode res
+
+ensureNATSIsListening :: DC.HostPort -> Int -> IO ()
+ensureNATSIsListening hp retryCount = do
+  result <- try(callNATSHealth hp retryCount) :: IO (Either SomeException HTTP.ResponseCode)
   case result of
     Left ex -> do
       if retryCount == 0
         then error $ "retried too many times " ++ show ex
       else do
         threadDelay $ second `div` 2
-        ensureNATSIsListening $ retryCount - 1
-    Right sock -> ensureNATSIsResponding sock retryCount
-
-ensureNATSIsResponding :: TCP.Socket -> Int -> IO ()
-ensureNATSIsResponding sock retryCount = do
-  result <- try(TCP.connect "0.0.0.0" "4222" $ \(sock, _) -> do
-    res <- TCP.recv sock 1000
-    TCP.closeSock sock
-    case res of
-      Just a  -> return ()
-      Nothing -> do
+        ensureNATSIsListening hp $ retryCount - 1
+    Right code -> case code of
+      (2, 0, 0) -> return ()
+      b         -> do
         if retryCount == 0
-          then error $ "retried too many times (no response)"
+          then error $ "retried too many times: status: " ++ show b
         else do
-          threadDelay $ second `div` 4
-          ensureNATSIsResponding sock $ retryCount-1
-    ) :: IO (Either SomeException ())
-  case result of
-    Left ex -> do
-      if retryCount == 0
-        then error $ "retried too many times " ++ show ex
-      else do
-        threadDelay $ second `div` 4
-        ensureNATSIsResponding sock $ retryCount-1
-    Right _ -> return ()
+          threadDelay $ second `div` 2
+          ensureNATSIsListening hp $ retryCount - 1
 
-startNATS :: IO DC.ContainerID
+startNATS :: IO (DC.ContainerID, String, Int)
 startNATS = do
-  id <- runNATSContainer
-  ensureNATS id
+  (id, d) <- runNATSContainer
+  ensureNATS (id, d)
 
-stopNATS :: DC.ContainerID -> IO ()
-stopNATS cid = do
+stopNATS :: (DC.ContainerID, String, Int) -> IO ()
+stopNATS (cid, _, _) = do
   h <- DC.unixHttpHandler "/var/run/docker.sock"
   DC.runDockerT (DC.defaultClientOpts, h) $
     do r <- DC.stopContainer DC.DefaultTimeout cid
