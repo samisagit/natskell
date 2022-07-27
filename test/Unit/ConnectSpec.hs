f27d4f3d064009527f23e4dac267cc758ca190e7
samisagit <sam@whiteteam.co.uk>
2022-07-27 08:04:42 +0000

    Move test fixtures to their own package

--- test/Unit/ConnectSpec.hs
+++ test/Unit/ConnectSpec.hs
@@ -11,6 +11,7 @@ import qualified Data.ByteString.Lazy      as LBS
 import           Data.Text                 (Text, pack)
 import           Data.Text.Encoding
 import qualified Docker.Client             as DC
+import           Fixtures
 import           Parsers.Parsers
 import           Test.Hspec
 import           Text.Printf
@@ -22,15 +23,6 @@ spec = do
   generated
   explicit
 
-boolCases = [True, False]
-maybeUserCases = [Just "samisagit", Just "sam@google.com", Nothing]
-maybePassCases = [Just "dsalkj09898(*)(UHJHI&*&*)(910", Nothing]
-maybeNameCases = [Just "natskell-client",  Nothing]
-maybeVersionCases = [Just "0.0.0", Just "v1.0.1", Just "13.0.0+123", Nothing]
-maybeSigCases = [Just "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQSflKxwRJSMeKKF2QT4fwpMeJf36POk6yJVadQssw5c", Nothing]
-maybeJwtCases = [Just "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c", Nothing]
-maybeIntCases = [Just 1, Nothing]
-maybeBoolCases = [Just True, Just False, Nothing]
 
 generated = parallel $ do
   describe "generated" $ do
@@ -38,31 +30,31 @@ generated = parallel $ do
       forM_ boolCases $ \verbosity ->
         forM_ boolCases $ \pedanticity ->
           forM_ boolCases $ \tlsRequirement ->
-            forM_ maybeJwtCases $ \authTokenOptions ->
-              forM_ maybeUserCases $ \userOptions ->
-                forM_ maybePassCases $ \passOptions ->
-                  forM_ maybeNameCases $ \nameOptions ->
-                    forM_ maybeIntCases $ \protocolOptions ->
-                      forM_ maybeBoolCases $ \echoOptions ->
-                        forM_ maybeSigCases $ \sigOptions ->
-                          forM_ maybeJwtCases $ \jwtOptions -> do
-                            let d = Connect verbosity pedanticity tlsRequirement authTokenOptions userOptions passOptions nameOptions "Haskell" 1 protocolOptions echoOptions sigOptions jwtOptions
+            forM_ (maybeify jwtCases) $ \authTokenOptions ->
+              forM_ (maybeify userCases) $ \userOptions ->
+                forM_ (maybeify passCases) $ \passOptions ->
+                  forM_ (maybeify nameCases) $ \nameOptions ->
+                    forM_ (maybeify intCases) $ \protocolOptions ->
+                      forM_ (maybeify boolCases) $ \echoOptions ->
+                        forM_ (maybeify sigCases) $ \sigOptions ->
+                          forM_ (maybeify jwtCases) $ \jwtOptions -> do
+                            let d = Connect verbosity pedanticity tlsRequirement (fmap pack authTokenOptions) (fmap pack userOptions) (fmap pack passOptions) (fmap pack nameOptions) "Haskell" 1 (fmap fromIntegral protocolOptions) echoOptions (fmap pack sigOptions) (fmap pack jwtOptions)
                             it (printf "transforms %v successfully" (show d)) $ \f -> do
                               let transformed = transform d
                               let proto = BS.take 7 transformed
                               let json = BS.drop 8 transformed
                               let expectedFields = BS.init $ foldr BS.append "" [
-                                    collapseMaybeStringField "auth_token" authTokenOptions,
+                                    collapseMaybeStringField "auth_token" $ fmap pack authTokenOptions,
                                     collapseMaybeBoolField "echo" echoOptions,
-                                    collapseMaybeStringField "jwt" jwtOptions,
+                                    collapseMaybeStringField "jwt" $ fmap pack jwtOptions,
                                     collapseMaybeStringField "lang" (Just "Haskell"),
-                                    collapseMaybeStringField "name" nameOptions,
-                                    collapseMaybeStringField "pass" passOptions,
+                                    collapseMaybeStringField "name" $ fmap pack nameOptions,
+                                    collapseMaybeStringField "pass" $ fmap pack passOptions,
                                     collapseMaybeBoolField "pedantic" (Just pedanticity),
-                                    collapseMaybeIntField "protocol" protocolOptions,
-                                    collapseMaybeStringField "sig" sigOptions,
+                                    collapseMaybeIntField "protocol" $ fmap fromIntegral protocolOptions,
+                                    collapseMaybeStringField "sig" $ fmap pack sigOptions,
                                     collapseMaybeBoolField "tls_required" (Just tlsRequirement),
-                                    collapseMaybeStringField "user" userOptions,
+                                    collapseMaybeStringField "user" $ fmap pack userOptions,
                                     collapseMaybeBoolField "verbose" (Just verbosity),
                                     collapseMaybeIntField "version" (Just 1)
                                     ]
@@ -106,20 +98,4 @@ explicit = parallel $ do
         let got = decode . LBS.fromStrict $ json :: Maybe Value
         proto `shouldBe` "CONNECT"
         got `shouldBe` want
-
-collapseMaybeStringField :: BS.ByteString -> Maybe Text -> BS.ByteString
-collapseMaybeStringField f v  = case v of
-  Nothing -> ""
-  Just a  -> foldr BS.append "" ["\"", f, "\":", "\"", encodeUtf8 a, "\","]
-
-collapseMaybeBoolField :: BS.ByteString -> Maybe Bool -> BS.ByteString
-collapseMaybeBoolField f v  = case v of
-  Nothing    -> ""
-  Just True  -> foldr BS.append "" ["\"", f, "\":", "true,"]
-  Just False -> foldr BS.append "" ["\"", f, "\":", "false,"]
-
-collapseMaybeIntField :: BS.ByteString -> Maybe Int -> BS.ByteString
-collapseMaybeIntField f v = case v of
-  Nothing -> ""
-  Just a  -> foldr BS.append "" ["\"", f, "\":", encodeUtf8 . pack . show $ a, ","]
 
