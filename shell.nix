f87431ee882fb5752f0cd46f0c2546658b703a4b
Sam White <sam@debian-BULLSEYE-live-builder-AMD64>
2022-08-07 14:36:22 +0100

    Don't compile HLS in CI

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
-{ nixpkgs ? import (fetchTarball "http://nixos.org/channels/nixos-22.11/nixexprs.tar.xz") {}, compiler ? "ghc92" }:
+{ nixpkgs ? import (fetchTarball "http://nixos.org/channels/nixos-22.11/nixexprs.tar.xz") {}, compiler ? "ghc92", hls ? true }:
 let
   inherit (nixpkgs) pkgs;
-  ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [haskell-language-server]);
+  ghc = if hls then pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [haskell-language-server]) else pkgs.haskell.packages.${compiler}.ghc;
 in
 pkgs.mkShell {
   buildInputs = [ ghc pkgs.zlib pkgs.stack pkgs.hlint ];
