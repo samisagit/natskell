79b73e5202c4b981305113b6cc5ff8e389a376ea
Sam White <sam@debian-BULLSEYE-live-builder-AMD64>
2022-12-09 13:30:16 +0000

    Use nix-shell for CI builds

--- shell.nix
+++ shell.nix
@@ -1,12 +1,10 @@
 { nixpkgs ? import (fetchTarball "http://nixos.org/channels/nixos-22.11/nixexprs.tar.xz") {}, compiler ? "ghc92" }:
 let
   inherit (nixpkgs) pkgs;
-  ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
-          haskell-language-server
-	  stack
-        ]);
+  ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [haskell-language-server]);
 in
 pkgs.mkShell {
   buildInputs = [ ghc pkgs.zlib pkgs.stack pkgs.hlint ];
   shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
+  NIX_PATH = "nixpkgs=" + pkgs.path;
 }
