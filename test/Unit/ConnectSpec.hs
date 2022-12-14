faddbe4ef8f00babe0ae0333f0d4c88e8302fcb8
samisagit <sam@whiteteam.co.uk>
2022-12-14 14:38:32 +0000

    Use explicit test cases for CONNECT

--- test/Unit/ConnectSpec.hs
+++ test/Unit/ConnectSpec.hs
@@ -22,39 +22,31 @@ spec :: Spec
 spec = do
   explicit
 
-explicitCases :: [Connect]
+explicitCases :: [(BS.ByteString, Connect)]
 explicitCases = [
-  Connect False False False Nothing Nothing Nothing Nothing "" 1 Nothing Nothing Nothing Nothing,
-  Connect True True True (Just "token") (Just "user") (Just "pass") (Just "name") "Haskell" 1 (Just 3) (Just True) (Just "sig") (Just "jwt")
+  (
+    "CONNECT {\"verbose\": false, \"pedantic\": false, \"tls_required\": false, \"lang\": \"Haskell\", \"version\": 1}",
+    Connect False False False Nothing Nothing Nothing Nothing "Haskell" 1 Nothing Nothing Nothing Nothing
+    ),
+  (
+    "CONNECT {\"verbose\": true, \"pedantic\": true, \"tls_required\": true, \"auth_token\": \"token\", \"user\": \"user\", \"pass\": \"pass\", \"name\": \"name\", \"lang\": \"Haskell\", \"version\": 1, \"protocol\": 3, \"echo\": true, \"sig\": \"sig\", \"jwt\": \"jwt\"}",
+    Connect True True True (Just "token") (Just "user") (Just "pass") (Just "name") "Haskell" 1 (Just 3) (Just True) (Just "sig") (Just "jwt")
+    )
   ]
 
--- TODO: we should make this JSON explicit rather than building fields
 explicit = parallel $ do
   describe "transformer" $ do
-    forM_ explicitCases $ \input -> do
+    forM_ explicitCases $ \(want, input) -> do
       it (printf "transforms %v successfully" (show input)) $ \f -> do
+
         let transformed = transform input
-        let proto = BS.take 7 transformed
-        let json = BS.drop 8 transformed
-        let expectedFields = BS.init $ foldr BS.append "FIX ME " [
---              collapseMaybeStringField "auth_token" $ auth_token input,
---              collapseMaybeBoolField "echo" $ echo input,
---              collapseMaybeStringField "jwt" $ jwt input,
---              collapseMaybeStringField "lang" (Just $ lang input),
---              collapseMaybeStringField "name" $ name input,
---              collapseMaybeStringField "pass" $ pass input,
---              collapseMaybeBoolField "pedantic" (Just $ pedantic input),
---              collapseMaybeIntField "protocol" $ protocol input,
---              collapseMaybeStringField "sig" $ sig input,
---              collapseMaybeBoolField "tls_required" (Just $ tls_required input),
---              collapseMaybeStringField "user" $ user input,
---              collapseMaybeBoolField "verbose" (Just $ verbose input),
---              collapseMaybeIntField "version" (Just $ version input)
-              ]
-        -- we don't really care about field order, nor do we want to ensure it in the test
-        -- so we'll just decode the want and the got into a Haskell type and check they're equal
-        let want = (decode . LBS.fromStrict $ foldr BS.append "" ["{", expectedFields, "}"]) :: Maybe Value
-        let got = decode . LBS.fromStrict $ json :: Maybe Value
-        proto `shouldBe` "CONNECT"
-        got `shouldBe` want
+
+        let gotProto = BS.take 7 transformed
+        let gotJSONString = BS.drop 8 transformed
+
+        let wantJSON = decode . LBS.fromStrict $ BS.drop 8 want :: Maybe Value
+        let gotJSON = decode . LBS.fromStrict $ gotJSONString :: Maybe Value
+
+        gotProto `shouldBe` "CONNECT"
+        gotJSON `shouldBe` wantJSON
 
