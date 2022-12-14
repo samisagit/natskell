c4bcbb7c6ba77fd8f34ffedac5dfe55f4e9a2f42
samisagit <sam@whiteteam.co.uk>
2022-12-14 21:07:02 +0000

    Remove unnecessary transformer tests

--- test/Unit/PubSpec.hs
+++ test/Unit/PubSpec.hs
@@ -2,13 +2,8 @@
 
 module PubSpec (spec) where
 
-import           ASCII                     as A
-import           ASCII.Char                as AC
 import           Control.Monad
 import qualified Data.ByteString           as BS
-import qualified Data.Text                 as T
-import           Data.Text.Encoding        (encodeUtf8)
-import           Fixtures
 import           Test.Hspec
 import           Text.Printf
 import           Transformers.Transformers
@@ -17,7 +12,6 @@ import           Types.Pub
 spec :: Spec
 spec = do
   explicit
-  generated
 
 cases :: [(Pub, BS.ByteString, String)]
 cases = [
@@ -32,32 +26,3 @@ explicit = parallel $ do
     it (printf "correctly transforms %s" caseName) $ do
       transform input `shouldBe` expected
 
-generated = parallel $ do
-  describe "generated" $ do
-    forM_ subjectCases $ \subject -> do
-      forM_ (maybeify subjectCases) $ \replyTo -> do
-        forM_ maxPayloadCases $ \byteCount -> do
-          let payload = payloadGen byteCount
-          let output = foldr BS.append "" [
-                "PUB ",
-                subject,
-                " ",
-                collapseMaybeBS replyTo " ",
-                intToByteString byteCount,
-                "\r\n",
-                collapseMaybeBS payload "\r\n"
-                ]
-          it (printf "correctly transforms %s" (show output)) $ do
-            transform (Pub subject replyTo payload) `shouldBe` output
-
-infASCII = cycle AC.allCharacters
-
-payloadGen n
-  | n == 0 = Nothing
-  | n > 0 = Just . encodeUtf8 . T.pack . map A.charToUnicode $ take n infASCII
-
-collapseMaybeBS :: Maybe BS.ByteString -> BS.ByteString -> BS.ByteString
-collapseMaybeBS (Just a) sep = BS.append a sep
-collapseMaybeBS Nothing _    = ""
-
-intToByteString = encodeUtf8 . T.pack . show
