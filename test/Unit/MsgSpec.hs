6660e2e1c9c74539275856f35d209933552aa30b
samisagit <sam@whiteteam.co.uk>
2022-12-30 10:28:05 +0000

    Add header parser for HSMG

--- test/Unit/MsgSpec.hs
+++ test/Unit/MsgSpec.hs
@@ -4,6 +4,7 @@ module MsgSpec (spec) where
 
 import           Control.Monad
 import qualified Data.ByteString    as BS
+import           Data.Maybe
 import qualified Data.Text          as T
 import           Data.Text.Encoding (encodeUtf8)
 import           Fixtures
@@ -12,19 +13,20 @@ import           Test.Hspec
 import           Text.Printf
 import           Types.Msg
 
-
 spec :: Spec
 spec = do
   cases
 
 explicitCases :: [(BS.ByteString, Msg)]
 explicitCases = [
-  ("MSG FOO 13 BAR 17\r\nsome payload bits\r\n", Msg "FOO" "13" (Just "BAR") 17 (Just "some payload bits")),
-  ("MSG FOO 13 BAR 0\r\n", Msg "FOO" "13" (Just "BAR") 0 Nothing),
-  ("MSG FOO 13 17\r\nsome payload bits\r\n", Msg "FOO" "13" Nothing 17 (Just"some payload bits")),
-  ("MSG FOO 13 0\r\n", Msg "FOO" "13" Nothing 0 Nothing),
-  ("MSG FOO 13 20\r\nmulti\r\nline\r\npayload\r\n", Msg "FOO" "13" Nothing 20 (Just "multi\r\nline\r\npayload")),
-  ("MSG FOO.BAR.BAZ 13 IN.*.BOX.> 0\r\n", Msg "FOO.BAR.BAZ" "13" (Just "IN.*.BOX.>") 0 Nothing)
+  ("MSG FOO 13 BAR 17\r\nsome payload bits\r\n", Msg "FOO" "13" (Just "BAR") 17 (Just "some payload bits") Nothing),
+  ("MSG FOO 13 BAR 0\r\n", Msg "FOO" "13" (Just "BAR") 0 Nothing Nothing),
+  ("MSG FOO 13 17\r\nsome payload bits\r\n", Msg "FOO" "13" Nothing 17 (Just"some payload bits") Nothing),
+  ("MSG FOO 13 0\r\n", Msg "FOO" "13" Nothing 0 Nothing Nothing),
+  ("MSG FOO 13 20\r\nmulti\r\nline\r\npayload\r\n", Msg "FOO" "13" Nothing 20 (Just "multi\r\nline\r\npayload") Nothing),
+  ("MSG FOO.BAR.BAZ 13 IN.*.BOX.> 0\r\n", Msg "FOO.BAR.BAZ" "13" (Just "IN.*.BOX.>") 0 Nothing Nothing),
+  ("HMSG FOO 13 BAR 24 41\r\nNATS/1.0\r\nKEY: VALUE\r\n\r\nsome payload bits\r\n", Msg "FOO" "13" (Just "BAR") 17 (Just "some payload bits") (Just [("KEY", "VALUE")])),
+  ("HMSG FOO 13 24 44\r\nNATS/1.0\r\nKEY: VALUE\r\n\r\nmulti\r\nline\r\npayload\r\n", Msg "FOO" "13" Nothing 20 (Just "multi\r\nline\r\npayload") (Just [("KEY", "VALUE")]))
   ]
 
 generatedCases :: [(BS.ByteString, Msg)]
@@ -35,6 +37,7 @@ generatedCases = zip (map buildProtoInput msgs) msgs
             <*> sidCases
             <*> maybeify subjectCases)
             <*> payloadCases -- because byteCount and payload are in valid pairs we needed to uncurry this
+            <*> maybeify headerCases
 
 cases = parallel $ do
   describe "generic parser" $ do
@@ -48,18 +51,34 @@ cases = parallel $ do
         output `shouldBe` Just (ParsedMsg want)
 
 buildProtoInput :: Msg -> BS.ByteString
-buildProtoInput m = foldr BS.append "" [
-  "MSG",
-  " ",
-  subject m,
-  " ",
-  sid m,
-  " ",
-  collapseNothing (replyTo m) " ",
-  packStr' (printf "%v" (byteCount m)),
-  "\r\n",
-  collapseNothing (payload m) "\r\n"
-  ]
+buildProtoInput m = do
+  let headerProto = buildHeaderInput . headers $ m
+  foldr BS.append "" [
+    if isNothing . headers $ m then "" else "H",
+    "MSG",
+    " ",
+    subject m,
+    " ",
+    sid m,
+    " ",
+    collapseNothing (replyTo m) " ",
+    if isNothing . headers $ m then "" else packStr' (printf "%v" (BS.length headerProto)),
+    if isNothing . headers $ m then "" else " ",
+    packStr' (printf "%v" (BS.length headerProto + byteCount m)),
+    if isNothing . headers $ m then "" else "\r\n",
+    headerProto,
+    if isJust . headers $ m then "" else "\r\n",
+    collapseNothing (payload m) "\r\n"
+    ]
+
+buildHeaderInput :: Maybe [(BS.ByteString, BS.ByteString)] -> BS.ByteString
+buildHeaderInput Nothing   = ""
+buildHeaderInput (Just xs) = foldr BS.append "" ["NATS/1.0\r\n", headerPairProto xs, "\r\n"]
+
+headerPairProto :: [(BS.ByteString, BS.ByteString)] -> BS.ByteString
+headerPairProto []          = ""
+headerPairProto ((k, v):xs) = BS.append (foldr BS.append "" [k, ": ", v, "\r\n"]) (headerPairProto xs)
+
 
 collapseNothing :: Maybe BS.ByteString -> BS.ByteString -> BS.ByteString
 collapseNothing mbs suffix = case mbs of
