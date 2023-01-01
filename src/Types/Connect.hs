a79e8df27d40a58921408dadc28c13cbd03bca55
samisagit <sam@whiteteam.co.uk>
2023-01-01 11:47:54 +0000

    Add validator fuzz tests and implementations

--- src/Types/Connect.hs
+++ src/Types/Connect.hs
@@ -1,13 +1,15 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Types.Connect where
 
 import           Control.Monad
 import           Data.Aeson
-import qualified Data.ByteString    as BS
-import qualified Data.Text          as T
-import qualified Data.Text.Encoding as E
+import qualified Data.ByteString       as BS
+import qualified Data.Text             as T
+import qualified Data.Text.Encoding    as E
 import           GHC.Generics
+import           Validators.Validators
 
 data Connect = Connect
   {
@@ -37,6 +39,19 @@ instance ToJSON BS.ByteString where
 instance FromJSON BS.ByteString where
   parseJSON (String x) = textToByteString x
   parseJSON _          = mzero
+
+instance Validator Connect where
+  validate c
+    | auth_token c == Just "" = Just "explicit empty auth token"
+    | user c == Just "" = Just "explicit empty user"
+    | pass c == Just "" = Just "explicit empty pass"
+    | name c == Just "" = Just "explicit empty name"
+    | lang c ==  "" = Just "explicit empty lang"
+    | version c == "" = Just "explicit empty version"
+    | protocol c `notElem` [Nothing, Just 0, Just 1] = Just "invalid protocol"
+    | sig c == Just "" = Just "explicit empty sig"
+    | jwt c == Just "" = Just "explicit empty jwt"
+    | otherwise = Nothing
 
 textToByteString :: MonadPlus m =>  T.Text -> m BS.ByteString
 textToByteString x = pure $ E.encodeUtf8 x
--- src/Types/Unsub.hs
+++ src/Types/Unsub.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Types.Unsub where
 
-import qualified Data.ByteString as BS
+import qualified Data.ByteString       as BS
+import           Validators.Validators
 
 data Unsub = Unsub
   { sid    :: BS.ByteString,
     maxMsg :: Maybe Int
   }
   deriving (Eq, Show)
+
+instance Validator Unsub where
+  validate u
+    | sid u == "" = Just "explicit empty sid"
+    | otherwise = Nothing
