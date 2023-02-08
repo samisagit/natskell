adf79b0459b60e3e359c0c78e991dedddcf8afc3
samisagit <sam@whiteteam.co.uk>
2023-02-08 07:17:32 +0000

    Add fuzz test for til
    Shows the limitation of the parser only understanding ascii chars
    but this is acceptable since NATS also only deal with ascii

--- test/Fuzz/ParserSpec.hs
+++ test/Fuzz/ParserSpec.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ParserSpec (spec) where
 
 import qualified Data.ByteString       as BS
 import qualified Data.ByteString.Char8 as B
+import           Data.Char
 import           Data.Either
+import qualified Data.Word8            as W8
 import qualified Lib.Parser            as Parser
 import           Test.Hspec
 import           Test.Hspec.QuickCheck (modifyMaxSuccess)
@@ -13,6 +17,10 @@ spec = do
   qc
 
 qc = do
+  describe "til" $ do
+    it "qq" $ do
+      let output = Parser.runParser (Parser.til W8._greater) "cheat code for greater than\59966"
+      output `shouldBe` Right (BS.unpack "cheat code for greater than", ">")
   describe "char" $ do
     modifyMaxSuccess (const 100000) $ do
       it "passes quick check" . property $
@@ -21,6 +29,11 @@ qc = do
     modifyMaxSuccess (const 100000) $ do
       it "passes quick check" . property $
         propString
+  describe "til" $ do
+    modifyMaxSuccess (const 100000) $ do
+      it "passes quick check" . property $
+        propTil
+  -- test take
 
 propChar :: Char -> String -> Bool
 propChar c s = do
@@ -44,4 +57,20 @@ propString p i = do
     stringToByteString = B.pack
     parserExpectation = stringToByteString p
     parserInput = stringToByteString i
+
+propTil :: Char -> String -> Bool
+propTil c i = do
+  let output = Parser.runParser (Parser.til tilChar) parserInput
+
+  case output of
+    Right (struct, rest) -> tilChar `notElem` struct
+    Left _               -> not (stringIsAscii i) || null i || c == head i || c `notElem` tail i
+  where
+    charToByteString c = B.pack [c]
+    stringToByteString = B.pack
+    tilChar = BS.head $ charToByteString c
+    parserInput = stringToByteString i
+
+stringIsAscii :: String -> Bool
+stringIsAscii = all isAscii
 
